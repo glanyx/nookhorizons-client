068e0aba521cea2636cd569003654d69ea6df0a8
Glanyx <mikekornet@live.co.uk>
2020-02-04 10:59:21 +0000

    banner addition

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import "./App.css";
 
-import { makeStyles } from '@material-ui/core';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import theme from './theme';
+import { makeStyles } from "@material-ui/core";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
 
-import Routes from './Routes';
-import { NavBar } from './components';
-import { Auth } from 'aws-amplify';
+import Routes from "./Routes";
+import { NavBar } from "./components";
+import { Auth } from "aws-amplify";
 
 const useStyles = makeStyles(() => ({
   wrapper: {
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/ac%20background.png'})`
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/ac%20background.png"})`
   },
   backdrop: {
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/backdrop.png'})`,
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/backdrop.png"})`
   },
   banner: {
-    width: '100%',
-    height: '150px',
-    backgroundColor: '#6666ff'
+    width: "100%",
+    height: "150px",
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/banner.png"})`
   }
 }));
 
 function App(props) {
-
   const classes = useStyles();
 
   const [isAuthenticated, userHasAuthenticated] = useState(false);
@@ -39,8 +38,8 @@ function App(props) {
     try {
       await Auth.currentSession();
       userHasAuthenticated(true);
-    } catch(e) {
-      if (e !== 'No current user') {
+    } catch (e) {
+      if (e !== "No current user") {
         alert(e);
       }
     }
@@ -50,20 +49,24 @@ function App(props) {
   async function handleLogout() {
     await Auth.signOut();
     userHasAuthenticated(false);
-    props.history.push('/login');
+    props.history.push("/login");
   }
 
   return (
-    !isAuthenticating &&
-    <MuiThemeProvider theme={theme}>
-      <div className={classes.wrapper}>
-        <div className={`App-header container ${classes.backdrop}`}>
-          <div className={classes.banner} />
-          <NavBar userProps={{ isAuthenticated, userHasAuthenticated }} onLogout={handleLogout}/>
-          <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+    !isAuthenticating && (
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.wrapper}>
+          <div className={`App-header container ${classes.backdrop}`}>
+            <div className={classes.banner} />
+            <NavBar
+              userProps={{ isAuthenticated, userHasAuthenticated }}
+              onLogout={handleLogout}
+            />
+            <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+          </div>
         </div>
-      </div>
-    </MuiThemeProvider>
+      </MuiThemeProvider>
+    )
   );
 }
 
