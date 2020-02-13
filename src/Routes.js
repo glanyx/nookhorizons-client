5ece944ff567a5b58f128bcf3d31e34f1e4eadfe
Glanyx <mikekornet@live.co.uk>
2020-02-13 18:23:36 +0000

    test for release

--- src/Routes.js
+++ src/Routes.js
@@ -4,10 +4,7 @@ import AppliedRoute from './AppliedRoute';
 import {
   Home,
   Login,
-  Register,
-  Market,
-  Collections,
-  Guides
+  Register
 } from "./containers";
 import NotFound from './NotFound';
 
@@ -17,9 +14,6 @@ function Routes({ appProps }) {
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <AppliedRoute path="/register" exact component={Register} appProps={appProps} />
-      <AppliedRoute path="/marketplace" exact component={Market} appProps={appProps} />
-      <AppliedRoute path="/collections" exact component={Collections} appProps={appProps} />
-      <AppliedRoute path="/guides" exact component={Guides} appProps={appProps} />
       <Route component={NotFound} />
     </Switch>
   );
